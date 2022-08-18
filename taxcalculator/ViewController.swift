2ffb7a60bf1c7d1d2a5fb9b7205004162facf875
Lydia <lydia.h.kang@gmail.com>
2022-08-18 10:44:46 -0700

    hi

--- taxcalculator/ViewController.swift
+++ taxcalculator/ViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var initialEntered: UITextField!
-    
+    @IBOutlet weak var textField: UITextField!
+    var enteredScore = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! SecondViewController
+        
+        destinationVC.enteredScore = self.enteredScore
+    }
 
 }
 
